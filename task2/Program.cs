5a551bb7cb6c24a841389c589038a42a2f8b019d
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-06-11 16:45:20 +0300

    Task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace task2
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace task2
 {
     internal static class Program
     {
         private static void Main()
         {
+            var fileInfo = new FileInfo("test.txt");
+            var fileStream = fileInfo.Create();
+            Console.WriteLine(fileInfo.FullName);
+            Console.WriteLine(fileInfo.Attributes.ToString());
+            Console.WriteLine(fileInfo.CreationTime);
+            fileStream.WriteByte((byte) 123);
+            var streamWriter = new StreamWriter(fileStream);
+            streamWriter.WriteLine("text line");
+            streamWriter.Close();
+            fileStream.Close();
+            fileStream = fileInfo.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            Console.WriteLine(fileStream.ReadByte());
+            var streamReader = new StreamReader(fileStream);
+            string output;
+            while ((output = streamReader.ReadLine()) != null)
+            {
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine(streamReader.ReadLine());
+            //fileInfo.Delete();
         }
     }
 }
